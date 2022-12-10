7f4d2e008c8f688f76b9d6366e774bcf80c4a894
Brianne Siemsen <briannesiemsen@gmail.com>
2022-12-09 18:22:01 -0700

    added basic info

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("test");
+let bits = ["home", "contact", "skills", "works", "resume"];
+
+function show(hi){
+    bits.map(test => {
+        switch(test){
+            case hi:
+                document.getElementById(test).removeAttribute("hidden");
+                break;
+            default:
+                document.getElementById(test).setAttribute("hidden", "");
+                break;
+        }
+    });
+}
