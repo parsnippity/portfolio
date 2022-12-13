7336aaa49af5c4593b2b79140d7b0c7140d392fc
Brianne Siemsen <briannesiemsen@gmail.com>
2022-12-12 18:24:13 -0700

    added fade-in and messed with colors

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-let bits = ["home", "contact", "skills", "works", "resume"];
+let bits = [{shown: "home", click: "homeClick", color:"#bfe4f2"}, {shown: "contact", click: "contactClick", color: "#a7c8d4"}, {shown: "skills", click: "contactClick", color: "#8fabb5"}, {shown: "works", click: "worksClick", color: "#778e97"}, {shown: "resume", click: "resumeClick", color: "#607279"}];
 
-function show(hi){
-    bits.map(test => {
-        switch(test){
-            case hi:
-                document.getElementById(test).removeAttribute("hidden");
-                break;
-            default:
-                document.getElementById(test).setAttribute("hidden", "");
-                break;
-        }
-    });
+function show(hi, ho){
+    document.getElementById(hi).removeAttribute("hidden");
+    if (hi != shown) document.getElementById(shown).setAttribute("hidden", "");
+    document.getElementById(hi).setAttribute("class", "fade-in-text");
+    document.getElementById(ho).style.backgroundColor = "#BFE4F2";
+    document.getElementById(shown).removeAttribute("class");
+    document.getElementById(shownClick).style.backgroundColor = "white";
+    shown = hi;
+    shownClick = ho;
 }
