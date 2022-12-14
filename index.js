1693fd9f895eca71e44d2a252630ad05904625fe
Brianne Siemsen <briannesiemsen@gmail.com>
2022-12-13 18:22:20 -0700

    made it look like folders

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-let bits = [{shown: "home", click: "homeClick", color:"#bfe4f2"}, {shown: "contact", click: "contactClick", color: "#a7c8d4"}, {shown: "skills", click: "contactClick", color: "#8fabb5"}, {shown: "works", click: "worksClick", color: "#778e97"}, {shown: "resume", click: "resumeClick", color: "#607279"}];
+let bits = [{main: "home", click: "homeClick", color:"#bfe4f2", textColor: "#386373"}, {main: "contact", click: "contactClick", color: "#a7c8d4", textColor: "#294954"}, {main: "skills", click: "skillsClick", color: "#8fabb5", textColor: "#1A2E36"}, {main: "works", click: "worksClick", color: "#778e97", textColor: "#6FC2E3"}, {main: "resume", click: "resumeClick", color: "#607279", textColor: "#60A8C4"}];
+let shown = bits[0];
+let current = bits[0];
 
-function show(hi, ho){
+function show(hi){
+    for(let item of bits){
+        if(hi == item.main){
+            current = item;
+        }
+    }
     document.getElementById(hi).removeAttribute("hidden");
-    if (hi != shown) document.getElementById(shown).setAttribute("hidden", "");
+    if (hi != shown.main) document.getElementById(shown.main).setAttribute("hidden", "");
+    if (hi != shown.main) document.getElementById(shown.click).style.borderBottom = `2px solid #376070`;
     document.getElementById(hi).setAttribute("class", "fade-in-text");
-    document.getElementById(ho).style.backgroundColor = "#BFE4F2";
-    document.getElementById(shown).removeAttribute("class");
-    document.getElementById(shownClick).style.backgroundColor = "white";
-    shown = hi;
-    shownClick = ho;
+    document.getElementById(current.click).style.borderBottom = `2px solid ${current.color}`;
+    document.body.style.backgroundColor = current.color;
+    document.getElementById(shown.main).removeAttribute("class");
+    shown = current;
 }
