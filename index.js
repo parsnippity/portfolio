a7a6e6eeafd5fc4b70a57148e56b6afb2ae5faa8
Brianne Siemsen <briannesiemsen@gmail.com>
2022-12-16 18:25:02 -0700

    worked on past projects

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let bits = [{main: "home", click: "homeClick", color:"#bfe4f2", textColor: "#f1d
 let shown = bits[0];
 let current = bits[0];
 //B7E1F0
+if(window.matchMedia("(max-width: 550px)").matches) document.getElementById("homeClick").style.borderBottom = `2px solid #284854`;
 
 function show(hi){
     for(let item of bits){
@@ -11,15 +12,22 @@ function show(hi){
     }
     document.getElementById(hi).removeAttribute("hidden");
     if (hi != shown.main) document.getElementById(shown.main).setAttribute("hidden", "");
-    if (hi != shown.main) document.getElementById(shown.click).style.borderBottom = `2px solid #376070`;
     document.getElementById(hi).setAttribute("class", "fade-in-text");
-    document.getElementById(current.click).style.borderBottom = `2px solid ${current.color}`;
-    document.body.style.backgroundColor = current.color;
-    document.getElementById(shown.main).removeAttribute("class");
     if(current.main == "resumeSkills"){
         document.getElementById("resumeSkills").style.display = "grid"
     } else {
         document.getElementById("resumeSkills").style.display = "none"
+    }
+    if(current.main == "works"){
+        document.getElementById("works").style.display = "flex"
+    } else {
+        document.getElementById("works").style.display = "none"
+    }
+    document.getElementById(shown.main).removeAttribute("class");
+    if(window.matchMedia("(max-width: 550px)").matches == false) {
+        if (hi != shown.main) document.getElementById(shown.click).style.borderBottom = `2px solid #376070`;
+        document.getElementById(current.click).style.borderBottom = `2px solid ${current.color}`;
+        document.body.style.backgroundColor = current.color;
     }
     shown = current;
 }
